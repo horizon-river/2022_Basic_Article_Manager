81b1208a53580e0dff609e7be3557a5e78cdd47d
horizon-river <kimpk0416@gmail.com>
2022-07-22 17:24:02 +0900

    로그인 기능 구현(중복 로그인 허용)

--- src/com/KoreaIT/java/BAM/controller/MemberController.java
+++ src/com/KoreaIT/java/BAM/controller/MemberController.java
@@ -13,6 +13,7 @@ public class MemberController extends Controller {
 	private List<Member> members;
 	private String cmd;
 	private String actionMethodName;
+	private Member loginedMember;
 	
 	public void doAction(String cmd, String actionMethodName) {
 		this.cmd = cmd;
@@ -22,12 +23,18 @@ public class MemberController extends Controller {
 		case "join":
 			doJoin();
 			break;
+		case "login":
+			doLogin();
+			break;
+		case "profile":
+			showProfile();
+			break;
 		default:
 			System.out.println("존재하지 않는 명령어 입니다.");
 			break;
 		}
 	}
-	
+
 	public MemberController(Scanner scan) {
 		this.scan = scan;
 		
@@ -35,7 +42,7 @@ public class MemberController extends Controller {
 	}
 	
 	private void doJoin() {
-		int id = members.get(members.size() - 1).id + 1;
+		int id = members.size() + 1;
 		String regDate = Util.getNowDateStr();
 		String loginId = null;
 		
@@ -75,6 +82,48 @@ public class MemberController extends Controller {
 		
 		System.out.printf("%d번 회원님 환영합니다.\n", id);
 		
+	}
+	
+	private void doLogin() {
+		System.out.printf("로그인 아이디 : ");
+		String loginId = scan.nextLine();
+		System.out.printf("로그인 비밀번호 : ");
+		String loginPw = scan.nextLine();
+		
+		Member member = getMemberByLoginId(loginId);
+		
+		if(member == null) {
+			System.out.println("일치하는 회원이 없습니다");
+			return;
+		}
+		
+		if(loginPw.equals(member.loginPw) == false) {
+			System.out.println("비밀번호를 다시 입력해주세요");
+			return;
+		}
+		
+		loginedMember = member;
+		System.out.printf("로그인 성공! %s님 환영합니다.\n", loginedMember.name);
+		
+	}
+	
+	private void showProfile() {
+		if(loginedMember != null) {
+			System.out.printf("로그인 아이디 : %s\n", loginedMember.loginId);
+			System.out.printf("이름 : %s\n", loginedMember.name);
+		}else {
+			System.out.println("로그아웃 상태입니다");
+		}
+	}
+	
+	private Member getMemberByLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+		
+		if(index == -1) {
+			return null;
+		}
+		
+		return members.get(index);
 	}
 	
 	private boolean isJoinableLoginId(String loginId) {
